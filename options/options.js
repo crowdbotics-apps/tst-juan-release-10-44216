d9e909394d1df5f2e16ecd590f0f64da856a2a24
Juan Cruz <juan@crowdbotics.com>
2023-10-30 12:17:58 -0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "tst-juan-release-10-44216",
-  url: "https://tst-juan-release-10-44216.botics.co",
-  api: "https://tst-juan-release-10-44216.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "tst-juan-release-10-44216",
+  "url": "https://tst-juan-release-10-44216.botics.co",
+  "api": "https://tst-juan-release-10-44216.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
